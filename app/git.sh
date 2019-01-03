8fc70ad678eb6fd0f025327282192a3189c5d91e
Kitsune Solar <kitsune.solar@gmail.com>
2019-01-03 04:24:58 +0300

    2019-01-03 01:24:58

--- app/git.sh
+++ app/git.sh
@@ -12,9 +12,9 @@
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.timestamp() {
-	timestamp="$( date -u '+%Y-%m-%d %T' )"
+    timestamp="$( date -u '+%Y-%m-%d %T' )"
 
-	echo ${timestamp}
+    echo ${timestamp}
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -22,9 +22,9 @@ function ext.timestamp() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.build.version() {
-	version="$( date -u '+%Y%m%d%H%M%S' )"
+    version="$( date -u '+%Y%m%d%H%M%S' )"
 
-	echo ${version}
+    echo ${version}
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -32,17 +32,17 @@ function ext.build.version() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.git.push() {
-	name="$( basename ${PWD} )"
-	timestamp="$( ext.timestamp )"
-	commit="$*"
+    name="$( basename ${PWD} )"
+    timestamp="$( ext.timestamp )"
+    commit="$*"
 
-	echo ""
-	echo "--- Pushing ${name}"
+    echo ""
+    echo "--- Pushing ${name}"
 
-	git add . && git commit -a -m "${timestamp}" -m "${commit}" && git push
+    git add . && git commit -a -m "${timestamp}" -m "${commit}" && git push
 
-	echo "--- Finished ${name}"
-	echo ""
+    echo "--- Finished ${name}"
+    echo ""
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -50,13 +50,13 @@ function ext.git.push() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.git.push.all() {
-	for i in `ls`; do
-		if [[ -d ${i}/.git ]]; then
-			cd ${i}
-			git.push
-			cd ..
-		fi
-	done
+    for i in `ls`; do
+        if [[ -d ${i}/.git ]]; then
+            cd ${i}
+            ext.git.push
+            cd ..
+        fi
+    done
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -64,30 +64,30 @@ function ext.git.push.all() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.git.push.version() {
-	tags="$( git tag --list )"
-	changes="$(git status --porcelain)"
+    tags="$( git tag --list )"
+    changes="$(git status --porcelain)"
 
-	if [[ -z "${changes}" ]]; then
-		count="0"
-	else
-		count="1"
-	fi
+    if [[ -z "${changes}" ]]; then
+        count="0"
+    else
+        count="1"
+    fi
 
-	if [[ -z "${1}" ]]; then
-		if [[ -z "${tags}" ]]; then
-			version="1.0.0"
-		else
-			tag=( $( git describe --abbrev=0 --tags | tr '.' ' ' ) )
-			major=${tag[1]}
-			minor=${tag[2]}
-			patch=${tag[3]}
-			version="${major}.${minor}.$(( ${patch} + ${count} ))"
-		fi
-	else
-		version="${1}"
-	fi
+    if [[ -z "${1}" ]]; then
+        if [[ -z "${tags}" ]]; then
+            version="1.0.0"
+        else
+            tag=( $( git describe --abbrev=0 --tags | tr '.' ' ' ) )
+            major=${tag[1]}
+            minor=${tag[2]}
+            patch=${tag[3]}
+            version="${major}.${minor}.$(( ${patch} + ${count} ))"
+        fi
+    else
+        version="${1}"
+    fi
 
-	git.push && git tag -a ${version} -m "Version ${version}" && git push origin ${version}
+    ext.git.push && git tag -a ${version} -m "Version ${version}" && git push origin ${version}
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -95,13 +95,13 @@ function ext.git.push.version() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.git.push.page() {
-	if [[ -z "${1}" ]]; then
-		branch="page-stable"
-	else
-		branch="${1}"
-	fi
+    if [[ -z "${1}" ]]; then
+        branch="page-stable"
+    else
+        branch="${1}"
+    fi
 
-	git.push && git checkout master && git merge ${branch} && git push && git checkout ${branch}
+    ext.git.push && git checkout master && git merge ${branch} && git push && git checkout ${branch}
 }
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -109,11 +109,11 @@ function ext.git.push.page() {
 # -------------------------------------------------------------------------------------------------------------------- #
 
 function ext.git.push.cdn() {
-	if [[ -z "${1}" ]]; then
-		branch="cdn-stable"
-	else
-		branch="${1}"
-	fi
+    if [[ -z "${1}" ]]; then
+        branch="cdn-stable"
+    else
+        branch="${1}"
+    fi
 
-	git.push && git checkout master && git merge ${branch} && git push && git checkout ${branch}
+    ext.git.push && git checkout master && git merge ${branch} && git push && git checkout ${branch}
 }
